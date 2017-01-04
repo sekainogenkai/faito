7d002766edb3abfab160cac06fffef4f3f0de472
Krisofer Brink <kpgbrink@gmail.com>
2017-01-03 21:53:12 -0500

    added animation. Use omi. it is has walk animation.

--- client/index.js
+++ client/index.js
@@ -8,9 +8,11 @@ const scene = new BABYLON.Scene(engine);
 scene.enablePhysics(
   new BABYLON.Vector3(0, -9.81, 0),
   new BABYLON.OimoJSPlugin());
-require('../models/simple.blend').Append(BABYLON.SceneLoader, scene, loadedScene => {
+for (var x in [0,1]) {
+require('../models/bumi.blend').Append(BABYLON.SceneLoader, scene, loadedScene => {
   /*onsuccess*/
-  new BABYLON.PhysicsImpostor(loadedScene.meshes[0], BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, }, scene);
+  new BABYLON.PhysicsImpostor(loadedScene.meshes[x], BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, }, scene);
 }, x => {/*onprogress*/}, ex => {/*onerror*/});
+}
 
 engine.runRenderLoop(() => scene.render());

104ca0401f0adf06b64cfe8e865cc4655fc26ed6
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2017-01-03 23:07:47 -0500

    Demonstrate BabylonJS animations working.

--- client/index.js
+++ client/index.js
@@ -6,13 +6,19 @@ const engine = new BABYLON.Engine(document.getElementById('render-canvas'), true
 window.addEventListener('resize', () => engine.resize());
 const scene = new BABYLON.Scene(engine);
 
+const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
+camera.setTarget(new BABYLON.Vector3.Zero());
+
+const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
+
 scene.enablePhysics(
   new BABYLON.Vector3(0, -9.81, 0),
   new BABYLON.OimoJSPlugin());
 for (var x in [0,1]) {
-require('../models/bumi.blend').Append(BABYLON.SceneLoader, scene, loadedScene => {
+require('../models/omi.blend').Append(BABYLON.SceneLoader, scene, loadedScene => {
   /*onsuccess*/
-  new BABYLON.PhysicsImpostor(loadedScene.meshes[x], BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, }, scene);
+//  new BABYLON.PhysicsImpostor(loadedScene.meshes[x], BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, }, scene);
+  loadedScene.beginAnimation(scene.skeletons[x], 0, 60, true, 2);
 }, x => {/*onprogress*/}, ex => {/*onerror*/});
 }
 

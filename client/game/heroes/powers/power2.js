82bc48e3693cfbc338e728b80d0427693390dbd9
Krisofer Brink <kpgbrink@gmail.com>
2017-03-04 13:40:36 -0500

    merge fixed

--- client/game/heroes/powers/power2.js
+++ client/game/heroes/powers/power2.js
@@ -43,11 +43,7 @@ export default class Power2 extends BasePower {
       const mesh = new BABYLON.Mesh.CreateSphere('mesh', meshSize, meshSize, this.game.scene);
       mesh.position.copyFrom(vectorStart);
       BABYLON.Tags.EnableFor(mesh);
-<<<<<<< HEAD
-      mesh.setPhysicsState(BABYLON.PhysicsEngine.SphereImpostor, {mass:99999999, friction:0.1, restitution:0.9});
-=======
       mesh.setPhysicsState(BABYLON.PhysicsEngine.SphereImpostor, {mass:10, friction:0.1, restitution:0.9});
->>>>>>> a1e789070226dcec61a03acb0c97a305380564c5
       mesh.physicsImpostor.physicsBody.collisionFilterGroup = this.game.scene.collisionGroupGround;
       mesh.physicsImpostor.physicsBody.collisionFilterMask = this.game.scene.collisionGroupNormal;
       this.game.scene.shadowGenerator.getShadowMap().renderList.push(mesh);
@@ -57,7 +53,7 @@ export default class Power2 extends BasePower {
       }
 
       // run spawn
-      new ProjectileObject(this.game, this.hero, mesh, vectorStart, vectorEnd, 10, secondsToTicks(5), powerImpulseVec, 100);
+      new ProjectileObject(this.game, this.hero, mesh, vectorStart, vectorEnd, 10, secondsToTicks(5), powerImpulseVec);
     }
 
     buttonDown(i) {
--- client/game/heroes/powers/powerObjects/projectileObject.js
+++ client/game/heroes/powers/powerObjects/projectileObject.js
@@ -6,21 +6,17 @@ import {getHeightAtCoordinates} from '../powerUtils/mainUtils';
 const matrix = new BABYLON.Matrix();
 
 export default class ProjectileObject extends BasePowerObject {
-  constructor(game, hero, mesh, vectorStart, vectorEnd, range, lifeSpan, vectorImpulse, mass=1) {
+  constructor(game, hero, mesh, vectorStart, vectorEnd, range, lifeSpan, vectorImpulse) {
     super(game, hero, mesh, vectorStart, vectorEnd, range, lifeSpan)
     this.vectorImpulse = vectorImpulse;
     this.directionVec = new BABYLON.Vector3(0, 0, 0);
-    this.mass = mass;
     // Get current rotation of player
     this.hero.mask.rotationQuaternion.toRotationMatrix(matrix);
   }
 
   onPowerSpawn() {
     this.mesh.physicsImpostor.physicsBody.type = 1; // Make the object kinematic
-<<<<<<< HEAD
-    this.mesh.physicsImpostor.physicsBody.mass = this.mass;
-=======
->>>>>>> a1e789070226dcec61a03acb0c97a305380564c5
+
     this.mesh.physicsImpostor.physicsBody.updateMassProperties();
     this.mesh.physicsImpostor.physicsBody.collisionFilterMask = this.game.scene.collisionGroupGround | this.game.scene.collisionGroupNormal;
 

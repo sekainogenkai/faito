698bc361d0eae9d8300b6dfb18a9e3177a06e781
Mark Wissink <mwissink.b@gmail.com>
2017-04-01 17:26:54 -0400

    fixed the menu camera

--- client/menu/menuCamera.js
+++ client/menu/menuCamera.js
@@ -4,37 +4,53 @@ export default class MenuCamera {
   constructor (game) {
     this.game = game;
     // Set the target vector which will get updated
-    this.initRadius = 100; // Used in setZoom
-    this.cameraSpeed = 5;
-    this.cameraTarget = new BABYLON.Vector3.Zero()
-    this.camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, 0.1, this.initRadius, this.cameraTarget, this.game.scene);
-    this.camera.setPosition(new BABYLON.Vector3(0, 40, -40));
     // Set radius values
     this.radius = {
-        current: 100,
-        target: 100,
-        min: 70,
+        max: 100,
+        min: 50
     };
+    this.cameraTarget = new BABYLON.Vector3.Zero()
+    this.camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, 0.1, this.radius.max, this.cameraTarget, this.game.scene);
+    this.camera.setPosition(new BABYLON.Vector3(0, 40, -40));
+
     // add animations
-    this.setAnimation(this.camera.alpha, 12, 1000);
-    this.setAnimation(this.camera.beta, 12, 1500);
+    this.setAnimation("alpha", 0, 12, 2500, "relative");
+    this.setAnimation("beta", 5, 5.5, 1000, "cycle");
+    this.setAnimation("radius", this.radius.max, this.radius.min, 2000, "cycle");
+
   }
 
-  setAnimation(prop, endVal, range=100,) {
-    var animCamera = new BABYLON.Animation("animCam", "alpha", 30,
-                                      BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-                                      BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT);
+  setAnimation(prop, startVal, endVal, range, loopType) {
     var keys = [];
+    var loopMode = undefined;
     keys.push({
         frame: 0,
-        value: prop
+        value: startVal
     });
-    keys.push({
-        frame: range,
-        value: endVal
-    });
+    if (loopType == "relative") {
+      keys.push({
+          frame: range,
+          value: endVal
+      });
+      loopMode = BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE;
+    } else if (loopType == "cycle") {
+      keys.push({
+          frame: range/2,
+          value: endVal
+      });
+      keys.push({
+          frame: range,
+          value: startVal
+      });
+      loopMode = BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE;
+    }
+    var animCamera = new BABYLON.Animation("anim" + prop, prop, 60,
+                                      BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+                                      loopMode);
+
+
     animCamera.setKeys(keys);
     this.camera.animations.push(animCamera);
-    this.game.scene.beginAnimation(this.camera, 0, range, false);
+    this.game.scene.beginAnimation(this.camera, 0, range, true);
   }
 }
